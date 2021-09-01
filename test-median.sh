e748f7c63c996c842a9b546afcc3f557c729ddfe
Emilio Silva Schlenker <e18r@disroot.org>
2021-09-01 08:44:57 -0500

    stale message check

--- test-median.sh
+++ test-median.sh
@@ -24,5 +24,9 @@ seth send $contract 'lift(address[])' [$ETH_FROM]
 echo ""
 
 echo "poke"
+seth send $contract 'poke(uint256[],uint256[],uint8[],bytes32[],bytes32[])' $(./sign.sh)
+echo ""
+
+echo "poke: stale message"
 seth call $contract 'poke(uint256[],uint256[],uint8[],bytes32[],bytes32[])' $(./sign.sh)
 echo ""
